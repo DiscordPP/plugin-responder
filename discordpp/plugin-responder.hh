05a7674c545a33b64e9e23f74d6badf1a8b041db
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2020-05-20 09:56:07 -0500

    Don't respond to self

--- discordpp/plugin-responder.hh
+++ discordpp/plugin-responder.hh
@@ -7,20 +7,37 @@
 
 #include <discordpp/plugin-overload.hh>
 
+#include <boost/lexical_cast.hpp>
+
 namespace discordpp{
 	template<class BASE>
 	class PluginResponder: public BASE, virtual BotStruct{
+		snowflake id_;
 	public:
 		std::string prefix = "!";
+
+		virtual void initBot(
+				unsigned int apiVersionIn, const std::string &tokenIn, std::shared_ptr<boost::asio::io_context> aiocIn
+		) override{
+			BASE::initBot(apiVersionIn, tokenIn, aiocIn);
+
+			handlers.insert(
+					{
+							"READY",
+							[this](json data){
+								id_ = boost::lexical_cast<snowflake>(data["user"]["id"].get<std::string>());
+							}
+					}
+			);
+		}
 
 		virtual void respond(const std::string& command, const std::function<void(json)> handler){
 			handlers.insert(
 					{
 							"MESSAGE_CREATE",
 							[this, command, handler](json msg){
-								// Check for the command
-								const std::string& content = msg["content"];
-								if(content.rfind(prefix + command, 0) == 0){
+								// Check for self and prefixed command
+								if(boost::lexical_cast<snowflake>(msg["author"]["id"].get<std::string>()) != id_ && msg["content"].get<std::string>().rfind(prefix + command, 0) == 0){
 									// Run the response
 									handler(msg);
 								}
