96e28f6b4e092b6d7e4089b57fce408139627ecd
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2020-08-05 00:51:23 -0500

    LLVM coding style with Clangd

--- discordpp/plugin-responder.hh
+++ discordpp/plugin-responder.hh
@@ -9,52 +9,48 @@
 
 #include <boost/lexical_cast.hpp>
 
-namespace discordpp{
-	template<class BASE>
-	class PluginResponder: public BASE, virtual BotStruct{
-		snowflake id_;
-	public:
-		std::string prefix = "!";
+namespace discordpp {
+template <class BASE> class PluginResponder : public BASE, virtual BotStruct {
+    snowflake id_;
 
-		virtual void initBot(
-				unsigned int apiVersionIn, const std::string &tokenIn, std::shared_ptr<boost::asio::io_context> aiocIn
-		) override{
-			BASE::initBot(apiVersionIn, tokenIn, aiocIn);
+  public:
+    std::string prefix = "!";
 
-			handlers.insert(
-					{
-							"READY",
-							[this](json data){
-								id_ = boost::lexical_cast<snowflake>(data["user"]["id"].get<std::string>());
-							}
-					}
-			);
-		}
+    virtual void
+    initBot(unsigned int apiVersionIn, const std::string &tokenIn,
+            std::shared_ptr<boost::asio::io_context> aiocIn) override {
+        BASE::initBot(apiVersionIn, tokenIn, aiocIn);
 
-		virtual void respond(const std::string& command, const handleEvent handler){
-			handlers.insert(
-					{
-							"MESSAGE_CREATE",
-							[this, command, handler](json msg){
-								// Check for self and prefixed command
-								if(boost::lexical_cast<snowflake>(msg["author"]["id"].get<std::string>()) != id_ && msg["content"].get<std::string>().rfind(prefix + command, 0) == 0){
-									// Run the response
-									handler(msg);
-								}
-							}
-					}
-			);
-		}
+        handlers.insert({"READY", [this](json data) {
+                             id_ = boost::lexical_cast<snowflake>(
+                                 data["user"]["id"].get<std::string>());
+                         }});
+    }
 
-		virtual void respond(const std::string& command, const std::string response){
-			respond(command, [this, response](json msg){
-				// Echo the created message
-				BASE::call(
-						"POST",
-						"/channels/" + msg["channel_id"].get<std::string>() + "/messages",
-						json({{"content", response}})
-				);
-			});
-		}
-	};
-}
+    virtual void respond(const std::string &command,
+                         const handleEvent handler) {
+        handlers.insert(
+            {"MESSAGE_CREATE", [this, command, handler](json msg) {
+                 // Check for self and prefixed command
+                 if (boost::lexical_cast<snowflake>(
+                         msg["author"]["id"].get<std::string>()) != id_ &&
+                     msg["content"].get<std::string>().rfind(prefix + command,
+                                                             0) == 0) {
+                     // Run the response
+                     handler(msg);
+                 }
+             }});
+    }
+
+    virtual void respond(const std::string &command,
+                         const std::string response) {
+        respond(command, [this, response](json msg) {
+            // Echo the created message
+            BASE::call("POST",
+                       "/channels/" + msg["channel_id"].get<std::string>() +
+                           "/messages",
+                       json({{"content", response}}));
+        });
+    }
+};
+} // namespace discordpp
